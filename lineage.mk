56242940603df08eeb57691553ec90320d446ecd
LuK1337 <priv.luk@gmail.com>
2017-01-01 23:08:12 +0100

    titan: cm -> lineage
    Change-Id: I37708b90480d8e7ccc2168e8c0dedefef6c3d4ef

--- lineage.mk
+++ lineage.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := cm_titan
+PRODUCT_NAME := lineage_titan
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
